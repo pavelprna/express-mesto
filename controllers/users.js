5a6f719f4f428e2884c1b7215ce159180acdbc2e
Pavel Zharov <pavel@prna.ru>
2021-09-15 21:49:07 +0700

    feat: :goal_net: catch user errors

--- controllers/users.js
+++ controllers/users.js
@@ -2,35 +2,74 @@ const User = require('../models/user');
 
 const getUsers = (req, res) => {
   User.find({})
-    .then(users => res.send({ data: users }))
+    .then(users => res.status(200).send({ data: users }))
+    .catch(() => res.status(500).send({ message: 'Ошибка при получении пользователей' }))
 }
 
 const getUser = (req, res) => {
   User.findById(req.params.id)
-    .then(user => res.send({ data: user}))
-    .catch(() => res.status(500).send({message: 'Произошла ошибка'}))
+    .then(user => {
+      if (user) {
+        res.status(200).send({ user })
+      } else {
+        res.status(404).send({ message: 'Пользователь по указанному _id не найден' })
+      }
+    })
+    .catch(() => res.status(500).send({ message: 'Произошла ошибка' }))
 }
 
 const createUser = (req, res) => {
   const { name, about, avatar } = req.body;
 
   User.create({ name, about, avatar })
-    .then(user => res.send({ data: user }))
-    .catch(() => res.status(500).send({ message: 'Произошла ошибка при создании пользователя', body: req.body }))
+    .then(user => res.send({ user }))
+    .catch((error) => {
+      if (error.name === "ValidationError") {
+        res.status(400).send({ message: 'Переданы некорректные данные при создании пользователя' })
+      } else {
+        res.status(500).send({ message: 'Ошибка при создании пользователя' })
+      }
+    })
 }
 
 const updateUser = (req, res) => {
   const { name, about } = req.body;
 
-  User.updateOne({ _id: req.user._id}, { name, about })
-    .then(user => res.send({ data: user }));
+  User.findByIdAndUpdate({ _id: req.user._id }, { name, about })
+    .then(user => {
+      if (user) {
+        res.status(200).send({ user });
+      } else {
+        res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
+      }
+    })
+    .catch((error) => {
+      if (error.name === "ValidationError") {
+        res.status(400).send({ message: 'Переданы некорректные данные при обновлении профиля' })
+      } else {
+        res.status(500).send({ message: 'Ошибка при обновлении профиля' })
+      }
+    });
 }
 
 const updateAvatar = (req, res) => {
   const { link } = req.body;
 
-  User.updateOne({ _id: req.user._id}, { avatar: link })
-    .then(user => res.send({ data: user }));
+  User.findByIdAndUpdate({ _id: req.user._id }, { avatar: link })
+  .then(user => {
+    if (user) {
+      res.status(200).send({ user });
+    } else {
+      res.status(404).send({ message: 'Пользователь с указанным _id не найден' });
+    }
+  })
+  .catch((error) => {
+    if (error.name === "ValidationError") {
+      res.status(400).send({ message: 'Переданы некорректные данные при обновлении аватара' })
+    } else {
+      res.status(500).send({ message: 'Ошибка при обновлении аватара' })
+    }
+  });
 }
 
 module.exports = {
