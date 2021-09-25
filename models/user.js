353b9e66b895ca42786868f30aa383dcd85d6cec
Pavel Zharov <pavel@prna.ru>
2021-09-25 11:15:36 +0700

    feat: :sparkles: add email and password in userSchema

--- models/user.js
+++ models/user.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const validator = require('validator');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -14,6 +15,18 @@ const userSchema = new mongoose.Schema({
     maxlength: 30,
   },
   avatar: {
+    type: String,
+    required: true,
+  },
+  email: {
+    type: String,
+    required: true,
+    validate: {
+      validator: validator.isEmail,
+      message: 'Неверный email',
+    },
+  },
+  password: {
     type: String,
     required: true,
   },
