51ea9f3a7387922e394ebcd0e906d8c8cab40dc5
Pavel Zharov <pavel@prna.ru>
2021-09-26 00:34:22 +0700

    feat: :sparkles: add login controller

--- app.js
+++ app.js
@@ -1,10 +1,13 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
 
 const { PORT = 3000 } = process.env;
 
 const app = express();
+
+app.use(cookieParser());
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
--- controllers/users.js
+++ controllers/users.js
@@ -1,4 +1,30 @@
+require('dotenv').config();
+const jwt = require('jsonwebtoken');
 const User = require('../models/user');
+
+const login = (req, res) => {
+  const { email, password } = req.body;
+
+  return User.findUserByCredentials(email, password)
+    .then((user) => {
+      const { NODE_ENV, JWT_SECRET } = process.env;
+
+      const token = jwt.sign(
+        { _id: user._id },
+        NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret',
+        { expiresIn: '7d' },
+      );
+
+      res.cookie('jwt', token, {
+        maxAge: 3600000 * 24 * 7,
+        httpOnly: true,
+      })
+        .end();
+    })
+    .catch((err) => {
+      res.status(401).send({ message: err.message });
+    });
+};
 
 const getUsers = (req, res) => {
   User.find({})
