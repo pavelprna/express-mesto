9baf8a489ad74c3cf81a501e1f1623d843f067a5
Pavel Zharov <pavel@prna.ru>
2021-09-27 00:03:38 +0700

    fix: :ambulance: fix auth with cookies

--- middlewares/auth.js
+++ middlewares/auth.js
@@ -3,20 +3,15 @@ const UnauthorizedError = require('../errors/unauthorized-error');
 
 // eslint-disable-next-line consistent-return
 module.exports = (req, res, next) => {
-  const { authorization } = req.headers;
-
-  if (!authorization || !authorization.startsWith('Bearer ')) {
-    return res.status(403).send({ message: 'Необходима авторизация' });
-  }
-
-  const token = authorization.replace('Bearer ', '');
-
+  const token = req.cookies.jwt;
   const { NODE_ENV, JWT_SECRET } = process.env;
+
   let payload;
+
   try {
     payload = jwt.verify(token, NODE_ENV === 'production' ? JWT_SECRET : 'dev-secret');
   } catch (err) {
-    throw new UnauthorizedError('Происланный токен некорректен');
+    throw new UnauthorizedError('Присланный токен некорректен');
   }
 
   req.user = payload;
